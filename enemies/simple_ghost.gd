e2d0630a89f811a849c31862380e04864ec42732
HoneyPony <honeyponygames@gmail.com>
2024-10-04 20:35:24 -0400

    Start working on enemy

---
+++ enemies/simple_ghost.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+func _physics_process(delta):
+	var players = get_tree().get_nodes_in_group("Players")
+	if players.is_empty():
+		return
+	
+	var to_pos = Vector2.ZERO
+	for p in players:
+		to_pos += p.global_position * randf_range(0.8, 1.2)
+		
+	to_pos /= players.size()
+	
+	var vel_dir = (to_pos - global_position).normalized()
+	velocity = vel_dir * 32
+	
+	move_and_slide()
+	
