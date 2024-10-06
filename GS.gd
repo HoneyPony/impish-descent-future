a5d05fc47d3696a459d42242964c2c0340d69253
HoneyPony <honeyponygames@gmail.com>
2024-10-05 20:07:29 -0400

    Try making the mood more interesting

--- GS.gd
+++ GS.gd
@@ -3,7 +3,12 @@ extends Node
 # The "global state" node. This is where global variables are usually stored,
 # as well as things like scene preloads.
 
-var Game = preload("res://Game.tscn")
+#var Game = preload("res://Game.tscn")
+
+var levels = [
+	preload("res://levels/level1.tscn")
+]
+
 var MainMenu = preload("res://MainMenu.tscn")
 
 var EnemyProjectile1 = preload("res://enemies/enemy_projectile1.tscn")
--- ui/MainMenu.gd
+++ ui/MainMenu.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_PlayButton_pressed():
-	get_tree().change_scene_to_packed(GS.Game)
+	get_tree().change_scene_to_packed(GS.levels[0])
